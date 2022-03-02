ac92e1a8454b9a0aa8afb4d741443a76b61b9fa7
Marek Buchler <mbuchler@amazon.com>
2022-03-01 21:55:03 -0800

    add validation of eventbridge outputs

--- backends/4-order-processing/statemachine/order_proc_sfn.test.js
+++ backends/4-order-processing/statemachine/order_proc_sfn.test.js
@@ -8,6 +8,7 @@ const AWS = require('aws-sdk')
 AWS.config.update({region: process.env.AWS_REGION})
 const sfn = new AWS.StepFunctions()
 const cfn = new AWS.CloudFormation()
+const sqs = new AWS.SQS()
 const sfnmock = require('./samtl_sfn_mock')
 const getWithRetry = require('./samtl_get_with_retry')
 const { fail } = require('assert')
@@ -38,6 +39,7 @@ const getStackOutput = async(stackName, outputName) => {
 // })
 
 test('sfn test1', async () => {
+    userId = Math.floor(Math.random() * (2**32 - 1))
     asl = JSON.parse(aslText)
 
     stateMachineArn = await getStackOutput(testStack, 'OrderProcessorStateMachine')
@@ -54,10 +56,10 @@ test('sfn test1', async () => {
     // updateStateMachine is eventually consistent so sleep a bit ... todo need a better way
     await new Promise(resolve => setTimeout(resolve, 2500))
 
-    console.log(`Starting execution for ${stateMachineArn}`)
+    console.log(`Starting execution for ${stateMachineArn}`) // or should we start it through the EB Event rule?
     execResp = await sfn.startExecution({
             stateMachineArn: stateMachineArn,
-            input: JSON.stringify({ 'detail': { 'userId': 100, 'orderId': 25}})
+            input: JSON.stringify({ detail: { userId: userId, orderId: 25 } })
         }).promise()
     console.log(`Execution started: ${JSON.stringify(execResp)}`)
 
@@ -68,10 +70,55 @@ test('sfn test1', async () => {
         return resp.status != 'RUNNING'
     }
     
-    return getWithRetry(getResult, checkResult, 5, new Date(new Date().getTime() + 5000))
+    await getWithRetry(getResult, checkResult, 5, new Date(new Date().getTime() + 5000))
         .then(resp => {
             console.log(`SFN Done ${JSON.stringify(resp)}`)
             expect(resp.status).toBe('SUCCEEDED')
         })
         .catch(err => fail(`Test failed: ${err}`))
-}, 10000)
+
+    testQueueURL = await getStackOutput(testStack, "TestEventBridgeListenerQueue")
+    console.log(`Retrieving message from test queue ${testQueueURL}`)
+
+    msgs = []
+
+    getMsgs = async () => {
+        recv = await sqs.receiveMessage({
+            AttributeNames: [ "SentTimestamp" ],
+            MaxNumberOfMessages: 10,
+            MessageAttributeNames: [ "All" ],
+            QueueUrl: testQueueURL,
+            VisibilityTimeout: 20,
+            WaitTimeSeconds: 5
+        }).promise()
+
+        console.log(`Received SQS msgs: ${JSON.stringify(recv)}`)
+
+        receipts = []
+        // filter user ids
+        recv.Messages.forEach(msg => {
+            body = JSON.parse(msg.Body)
+            console.log(`Parsed body to be ${JSON.stringify(body)}`)
+            console.log(`Found ${body.detail.userId}, looking for ${userId}`)
+            if(body.detail.userId == userId) {
+                msgs.push(body)
+                receipts.push({ Id: receipts.length, ReceiptHandle: msg.ReceiptHandle })
+            }
+        })
+
+        if(receipts.length > 0) { // delete messages that belong to this test
+            console.log(`Deleting ${receipts.length} SQS msgs`)
+            await sqs.deleteMessageBatch({ Entries: receipts, QueueUrl: testQueueURL }).promise()
+        }
+
+        return msgs
+    }
+
+    await getWithRetry(getMsgs, (m) => m.length >= 2, 5, new Date(new Date().getTime() + 20000))
+
+    // we're scoped down to our userId, should have exactly two messages
+    expect(msgs.length).toBe(2)
+
+    detailTypes = msgs.map(x => x['detail-type'])
+    expect(detailTypes.sort()).toEqual(["OrderProcessor.ShopUnavailable", "OrderProcessor.orderFinished"])
+}, 60*1000)
