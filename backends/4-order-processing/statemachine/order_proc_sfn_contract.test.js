2ad8b2dc82f18994cc4db8f3cb6b0f38582c2d32
Marek Buchler <mbuchler@amazon.com>
2022-03-07 14:49:34 -0800

    add contract test

---
+++ backends/4-order-processing/statemachine/order_proc_sfn_contract.test.js
@@ -0,0 +1,65 @@
+// aws ssm put-parameter --name /Serverlesspresso/dummy --value dummy --type String
+
+// Load SFN definition -- could also get from SFN itself 
+fs = require('fs')
+asl = JSON.parse(fs.readFileSync('op.asl.json'))
+
+const AWS = require('aws-sdk')
+AWS.config.update({region: process.env.AWS_REGION})
+const eventbridge = new AWS.EventBridge()
+const samtl = require('./samtl')
+const { fail } = require('assert')
+
+testStack = process.env.SAMTL_TEST_STACK || 'slscoffee-4'
+
+test('Test shop open but at capacity contract test', async () => {
+    userId = Math.floor(Math.random() * (2**32 - 1))
+    busName = await samtl.getStackParameter(testStack, "CoreEventBusName")
+    stateMachineArn = await samtl.getStackOutput(testStack, 'OrderProcessorStateMachine')
+
+    // set up state machine with mocks
+    samtl.stepFnMockTaskState(asl, 'Get Shop Status', [
+        { response: { statusCode: 200, body: JSON.stringify({ storeOpen: true }), isBase64Encoded: false } }
+    ])
+    samtl.stepFnMockTaskState(asl, 'Get Capacity Status', { response: { isCapacityAvailable: false } })
+    await samtl.stepFnUpdate(stateMachineArn, JSON.stringify(asl))
+
+    // trigger test test
+    const params = { Entries: [{
+        Detail: JSON.stringify({
+          orderId: 'order' + userId,
+          userId,
+          robot: true,
+          bucket: {}
+        }),
+        DetailType: 'Validator.NewOrder',
+        EventBusName: busName,
+        Source: 'awsserverlessda.serverlesspresso',
+        Time: new Date
+      }] 
+    }
+    const response = await eventbridge.putEvents(params).promise()
+    console.log('EventBridge putEvents:', response)    
+
+    // check that two EventBridge messages with the correct detail-type were generated
+    const isMessageForThisTest = (msg) => {
+        body = JSON.parse(msg)
+        console.log(`Parsed body to be ${JSON.stringify(body)}, checking userid match for ${userId}`)
+        return body['detail-type'] != "Validator.NewOrder" && body.detail.userId == userId
+    }
+
+    msgs = await samtl.receiveEventBridgeMessage(
+        testStack, 
+        busName, 
+        isMessageForThisTest, 
+        2 /* expected messages */, 
+        20 /* timout seconds */)
+
+    console.log(`Got msgs ${JSON.stringify(msgs)}`)
+
+    // we're scoped down to our userId, should have exactly two messages
+    expect(msgs.length).toBe(2)
+
+    detailTypes = msgs.map(x => JSON.parse(x)['detail-type'])
+    expect(detailTypes.sort()).toEqual(["OrderProcessor.ShopUnavailable", "OrderProcessor.orderFinished"])
+}, 60*1000)
